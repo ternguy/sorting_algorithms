0cade552c2cde7e66b9df6ddf2fae8ba6ac13ee4
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-11-07 07:23:52 -0800

    done

--- sort.h
+++ sort.h
@@ -1,56 +1,51 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef DECK_K
+#define DECK_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
-/* Comparison direction macros for bitonic sort */
-#define UP 0
-#define DOWN 1
-
 /**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
  */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
+typedef enum kind_e
+
+
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
 
 /**
- * struct listint_s - Doubly linked list node
+ * struct card_s - Playing card
  *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of thelist
-
- * @next: Pointer to the next element of the list
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
  */
-typedef struct listint_s
+typedef struct card_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
+	const char *value;
+	const kind_t kind;
+} card_t;
 
-/* Printing helper functions */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
 
-/* Sorting algoritms */
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list)
+void sort_deck(deck_node_t **deck);
 
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-
-#endif /* SORT_H */
+#endif /* DECK_H */
